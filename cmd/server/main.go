ac7ad5d8b069d94584f3c9ae2f32bd9d0d2c4031
Dmitriy <38376575+0db0@users.noreply.github.com>
2025-01-24 11:56:33 +0400

    feature-5: add docs (#4)
    Co-authored-by: Dmitry Budko <budkodvm@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,18 @@ import (
 	"metric-server/internal/app"
 )
 
+//	@title			Metric Collector API
+//	@version		0.1
+//	@description	Metrics and Alerting Service
+//	@termsOfService	http://swagger.io/terms/
+//	@contact.name	Metric Collector API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	budkodmv@gmail.com
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host		localhost:8889
+// @BasePath	/v0.1
 func main() {
 	cfg := config.MustLoad()
 	app.Run(cfg)
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,7 +3,9 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 	"metric-server/internal/adapters/http/api_v01"
+	_ "metric-server/swagger"
 )
 
 func New(v01 *api_v01.MetricAdapter) *chi.Mux {
@@ -21,10 +23,12 @@ func New(v01 *api_v01.MetricAdapter) *chi.Mux {
 		r.Post("/value", v01.GetMetric)
 
 		// actual POST /update. POST /update/{metric-type/{metric-name}/{value} stay for backward compatibility
-		r.Post("/update/{metric-type}/{metric-name}/{value}", v01.Collect)
+		r.Post("/update/{metric-type}/{metric-name}/{value}", v01.CollectFromPath)
 		r.Post("/update", v01.Collect)
 
 		r.Post("/updates", v01.CollectMany)
+
+		r.Get("/swagger/*", httpSwagger.WrapHandler)
 	})
 
 	return r
